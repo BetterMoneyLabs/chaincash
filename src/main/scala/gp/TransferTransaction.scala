c7718b95e8c50e50b6d8352055f6c5f0ea129f3c
amansingh-001 <amannsingh225@gmail.com>
2025-12-13 21:13:04 +0530

    Delete src directory

--- src/main/scala/gp/TransferTransaction.scala
+++
@@ -1,4 +0,0 @@
-package gp
-
-// Transfer is like on-chain UTXO transaction, multiple possible inputs and outputs, and only inputs signed
-case class TransferTransaction(inputs: Seq[Note], outputs: Seq[Note])
