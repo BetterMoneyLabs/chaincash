d578d75eb4dd4bd723725dd5542aece25ac509a8
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2026-03-24 14:08:22 +0300

    BasisDeployerSpec update

--- src/test/scala/chaincash/contracts/BasisDeployerSpec.scala
+++ src/test/scala/chaincash/contracts/BasisDeployerSpec.scala
@@ -24,28 +24,19 @@ class BasisDeployerSpec extends PropSpec with Matchers {
 
   property("BasisDeployer should create valid scan request") {
     val exampleReserveTokenId = "fedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210"
-    
+
     val scanRequest = BasisDeployer.createBasisScanRequest(exampleReserveTokenId)
-    
+
     scanRequest should include("scanName")
     scanRequest should include("Basis Reserve")
     scanRequest should include(exampleReserveTokenId)
     scanRequest should include("containsAsset")
   }
 
-  property("BasisDeployer should calculate redemption fees correctly") {
-    BasisConstants.calculateRedemptionFee(1000000000L) shouldBe 20000000L // 2% of 1 ERG
-    BasisConstants.calculateRedemptionFee(500000000L) shouldBe 10000000L  // 2% of 0.5 ERG
-    
-    BasisConstants.calculateNetRedemption(1000000000L) shouldBe 980000000L // 1 ERG - 2%
-    BasisConstants.calculateNetRedemption(500000000L) shouldBe 490000000L  // 0.5 ERG - 2%
-  }
-
   property("BasisDeployer should have correct constants") {
     BasisConstants.REDEEM_ACTION shouldBe 0
     BasisConstants.TOP_UP_ACTION shouldBe 1
-    BasisConstants.MIN_TOP_UP_AMOUNT shouldBe 1000000000L
+    BasisConstants.MIN_TOP_UP_AMOUNT shouldBe 100000000L // 0.1 ERG
     BasisConstants.EMERGENCY_REDEMPTION_TIME_IN_BLOCKS shouldBe 2160 // 3 days in blocks (3 * 720)
-    BasisConstants.REDEMPTION_FEE_PERCENTAGE shouldBe 2
   }
 }
